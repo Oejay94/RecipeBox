719764a8a26a06392d50a392cb37fe407ce84f64
Joseph Brown <joseph.d.brown94@gmail.com>
2020-02-03 15:14:29 -0500

    fixed syntax error for decsription for class AddRecipe in forms.py

--- Recipe/forms.py
+++ Recipe/forms.py
@@ -4,7 +4,7 @@ from Recipe.models import Author
 
 class AddRecipe(forms.Form):
     title = forms.CharField(max_length=30)
-    description = forms.Textarea()
+    description = forms.CharField(widget=forms.Textarea)
     instructions = forms.CharField(widget=forms.Textarea)
     author = forms.ModelChoiceField(queryset=Author.objects.all())
 
